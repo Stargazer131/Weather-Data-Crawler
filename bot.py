cf78e54bf6e999a91509069011e6ba60aa654d81
HAO <naotomori131@gmail.com>
2023-11-12 14:43:37 +0700

    add alert command

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import threading
-import time as tme
-from datetime import datetime, timedelta, time
+import time
+from datetime import datetime, timedelta
 
 import numpy as np
 from telegram import Update
@@ -17,7 +17,7 @@ latest_data = (datetime.now(), 75, 70, 1000, 5)
 
 def fetch_data():
     global latest_data
-    tme.sleep(2)
+    time.sleep(2)
     while True:
         timestamp, temp, humid = get_sensor_data()
         timestamp += timedelta(hours=7)
@@ -28,7 +28,7 @@ def fetch_data():
             press, wind = get_api_data(timestamp)
         latest_data = (timestamp, temp, humid, press, wind)
         print('get latest data')
-        tme.sleep(30)
+        time.sleep(30)
 
 
 # for printing emoji
@@ -46,7 +46,9 @@ icon_bank = {
         "wind_speed": "\U0001F32C",
         "humidity": "\U0001F4A7",
         "pressure": "\U0001F4A8",
-        "rain_prob": "\U00002614"
+        "rain_prob": "\U00002614",
+        "fog_prob": "\U0001F301",
+        "thunder_prob": "\U000026A1"
     },
 
     "logo": {
@@ -324,15 +326,14 @@ async def stop_daily_command(update: Update, context: ContextTypes.DEFAULT_TYPE)
 
 
 async def alert_handler(context: ContextTypes.DEFAULT_TYPE):
-    data = get_current_weather(latest_data)
+    data = get_next_hour_prediction(latest_data)
 
     answer = (
         f"The weather in next hour will be: {data['condition']} {icon_bank['weather'][data['icon']]}\n"
         f"Temperature: {data['temperature']:.1f}°C {icon_bank['weather']['temperature']}\n"
-        f"Wind speed: {data['wind_speed']:.1f} mps {icon_bank['weather']['wind_speed']}\n"
-        f"Humidity: {data['humidity']:.1f}% {icon_bank['weather']['humidity']}\n"
-        f"Pressure: {data['pressure']:.1f} hPa {icon_bank['weather']['pressure']}\n"
         f"Rain Probability: {data['rain_prob']:.2f}% {icon_bank['weather']['rain_prob']}\n"
+        f"Thunderstorm Probability: {data['thunder_prob']:.2f}% {icon_bank['weather']['thunder_prob']}\n"
+        f"Fog Probability: {data['fog_prob']:.2f}% {icon_bank['weather']['fog_prob']}\n"
         f"Please be careful!"
     )
 
@@ -341,34 +342,80 @@ async def alert_handler(context: ContextTypes.DEFAULT_TYPE):
 
 async def alert_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.message.chat_id
-    jobs = context.job_queue.get_jobs_by_name(f'daily_{chat_id}')
+    jobs = context.job_queue.get_jobs_by_name(f'alert_{chat_id}')
     if len(jobs) != 0:
-        text = 'You already subscribed to daily update'
+        text = 'You already subscribed to alert update'
     else:
-        now = datetime.now()
-        target_time = datetime(now.year, now.month, now.day, 7, 0, 0)
-        if now > target_time:
-            target_time += timedelta(days=1)
-        total_second_till_next_7am = (target_time - now).total_seconds()
-        context.job_queue.run_repeating(daily_handler, interval=60*60*24, first=2,
-                                        chat_id=chat_id, name=f'daily_{chat_id}')
-        text = 'You will receive new weather data every 07:00 AM'
+        context.job_queue.run_repeating(alert_handler, interval=60*60, first=2,
+                                        chat_id=chat_id, name=f'alert_{chat_id}')
+        text = 'You will be alerted for uncomfortable/hazardous weather every hour'
 
     await context.bot.send_message(chat_id=chat_id, text=text)
 
 
 async def stop_alert_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.message.chat_id
-    jobs = context.job_queue.get_jobs_by_name(f'daily_{chat_id}')
+    jobs = context.job_queue.get_jobs_by_name(f'alert_{chat_id}')
     if len(jobs) != 0:
         job = jobs[0]
         job.enabled = False
         job.schedule_removal()
-        text = 'Stop receiving daily update!'
+        text = 'Stop receiving alert update!'
     else:
-        text = "You haven't subscribed to daily update!"
+        text = "You haven't subscribed to alert update!"
 
     await context.bot.send_message(chat_id=chat_id, text=text)
+
+
+def get_next_hour_prediction(input_data: tuple):
+    timestamp, temp, humid, press, wind = input_data
+    inputs = [temp, humid, press, wind,
+              timestamp.month, timestamp.day, timestamp.hour]
+    inputs = np.array(inputs).reshape(1, -1)
+    classifier = all_models['classification']
+    hour = 1
+    data = {}
+
+    # temperature
+    model = all_models['regression']['hours'][f'{hour}_temperature']
+    predict_temperature = model.predict(inputs)[0]
+    data['temperature'] = (predict_temperature-32) * 5/9
+
+    # humidity
+    model = all_models['regression']['hours'][f'{hour}_humidity']
+    predict_humidity = model.predict(inputs)[0]
+    data['humidity'] = predict_humidity
+
+    # pressure
+    model = all_models['regression']['hours'][f'{hour}_pressure']
+    predict_pressure = model.predict(inputs)[0]
+    data['pressure'] = predict_pressure
+
+    # wind_speed
+    model = all_models['regression']['hours'][f'{hour}_wind_speed']
+    predict_wind_speed = model.predict(inputs)[0]
+    data['wind_speed'] = predict_wind_speed * 0.44704
+
+    # condition and probability
+    temp_inputs = [predict_temperature, predict_humidity, predict_pressure, predict_wind_speed,
+                   timestamp.month, timestamp.day, timestamp.hour+hour]
+    temp_inputs = np.array(temp_inputs).reshape(1, -1)
+
+    condition = inverse_transform_condition(classifier.predict(temp_inputs)[0]).lower()
+    fog_prob = classifier.predict_proba(temp_inputs)[0][2] * 100
+    rain_prob = classifier.predict_proba(temp_inputs)[0][3] * 100
+    thunder_prob = classifier.predict_proba(temp_inputs)[0][4] * 100
+    data['rain_prob'] = rain_prob
+    data['thunder_prob'] = thunder_prob
+    data['fog_prob'] = fog_prob
+    time_of_day = day_or_night(timestamp.hour, hour)
+    data['condition'] = condition
+    if condition == 'fair':
+        data['icon'] = time_of_day
+    else:
+        data['icon'] = condition
+
+    return data
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -401,27 +448,27 @@ def load_all_models():
     models['classification'] = model
     print('finish load classification model')
 
-    # # hours regression
-    # hours = [1, 3, 6, 9, 12]
-    # features = ['temperature', 'humidity', 'pressure', 'wind_speed']
-    # for hour in hours:
-    #     for feature in features:
-    #         file_path = f"model\\regression\\hours\\{hour}_{feature}_xgboost_regression.json"
-    #         model = XGBRegressor()
-    #         model.load_model(file_path)
-    #         models['regression']['hours'][f'{hour}_{feature}'] = model
-    #         print(f'finish load regression model ({hour}H-{feature})')
-    #
-    # # days regression
-    # days = list(range(1, 8))
-    # features = ['temperature', 'humidity', 'pressure', 'wind_speed']
-    # for day in days:
-    #     for feature in features:
-    #         file_path = f"model\\regression\\days\\{day}_{feature}_xgboost_regression.json"
-    #         model = XGBRegressor()
-    #         model.load_model(file_path)
-    #         models['regression']['days'][f'{day}_{feature}'] = model
-    #         print(f'finish load regression model ({day}D-{feature})')
+    # hours regression
+    hours = [1, 3, 6, 9, 12]
+    features = ['temperature', 'humidity', 'pressure', 'wind_speed']
+    for hour in hours:
+        for feature in features:
+            file_path = f"model\\regression\\hours\\{hour}_{feature}_xgboost_regression.json"
+            model = XGBRegressor()
+            model.load_model(file_path)
+            models['regression']['hours'][f'{hour}_{feature}'] = model
+            print(f'finish load regression model ({hour}H-{feature})')
+
+    # days regression
+    days = list(range(1, 8))
+    features = ['temperature', 'humidity', 'pressure', 'wind_speed']
+    for day in days:
+        for feature in features:
+            file_path = f"model\\regression\\days\\{day}_{feature}_xgboost_regression.json"
+            model = XGBRegressor()
+            model.load_model(file_path)
+            models['regression']['days'][f'{day}_{feature}'] = model
+            print(f'finish load regression model ({day}D-{feature})')
 
     end_time = datetime.now()
     print(end_time - start_time)
@@ -446,6 +493,8 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler("day", day_command))
     app.add_handler(CommandHandler("daily", daily_command))
     app.add_handler(CommandHandler("stop_daily", stop_daily_command))
+    app.add_handler(CommandHandler("alert", alert_command))
+    app.add_handler(CommandHandler("stop_alert", stop_alert_command))
 
     # message
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
